24d683ccdc7bd3be177363801b2c1ab5c99fd85e
Giovanny <giovanny@hotmail.com.br>
2017-12-31 14:04:01 -0200

    Moving some method to tools module

--- src/nointro-tool.py
+++ src/nointro-tool.py
@@ -62,88 +62,21 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-class RomTags(object):
-    def __init__(self, rom):
-        self.total = rom.name.count("(")
-        self.tags = self.__tag_array__(rom.name)
-
-    def __tag_array__(self, name):
-        tags = []
-        end = 0
-        for i in range(self.total):
-            start = name.find("(", end)
-            end = name.find(")", end + 1)
-            tags.extend(self.__strip_tag__(name[start + 1:end].split(",")))
-        return tags
-
-    def __strip_tag__(self, tags):
-        return [tag.strip() for tag in tags]
-
-    def __str__(self):
-        return self.tags.__str__()
-
-class RomCleanName(object):
-    def __init__(self, rom):
-        idx = rom.name.find("(")
-        self.name = rom.name[:idx - 1]
-
-    def __str__(self):
-        return self.name
-
-def load_md5_dict(roms):
-    md5_dict = {}
-
-    for rom in roms:
-        md5_dict[rom.md5] = rom
-
-    return md5_dict
-
-def filter_roms(roms, filterss):
-    filtered_roms = []
-
-    for rom in roms:
-        if should_filter(RomTags(rom).tags, filterss):
-            filtered_roms.append(rom)
-
-    return filtered_roms
-
-def should_filter(tags, filterss):
-    ret2 = True
-    for filters in filterss:
-        ret1 = False
-        for filter in filters:
-            if filter.startswith("!"):
-                ret1 |= not tags.__contains__(filter[1:])
-            else:
-                ret1 |= tags.__contains__(filter)
-        ret2 &= ret1
-    return ret2
-
-def unique_filter(roms):
-    unique_names = []
-    unique_roms = []
-    for rom in roms:
-        unique_name = RomCleanName(rom).name
-        if unique_name not in unique_names:
-            unique_names.append(unique_name)
-            unique_roms.append(rom)
-    return unique_roms
-
 nointro_roms = tools.load_nointro_roms(args.dat_files)
 roms = tools.load_roms(args.rom_folders)
 
-nointro_roms = filter_roms(nointro_roms, args.filters)
+nointro_roms = tools.filter_roms(nointro_roms, args.filters)
 
 if (args.unique):
-    nointro_roms = unique_filter(nointro_roms)
+    nointro_roms = tools.unique_filter(nointro_roms)
 
-roms = filter_roms(roms, args.filters)
+roms = tools.filter_roms(roms, args.filters)
 
 if (args.unique):
-    roms = unique_filter(roms)
+    roms = tools.unique_filter(roms)
 
-nointro_rom_md5 = load_md5_dict(nointro_roms)
-rom_md5 = load_md5_dict(roms)
+nointro_rom_md5 = tools.load_md5_dict(nointro_roms)
+rom_md5 = tools.load_md5_dict(roms)
 
 if args.action == "status":
     print("Loaded {0} no-intro rom file(s)".format(len(nointro_roms)))
--- src/tools.py
+++ src/tools.py
@@ -1,6 +1,34 @@
 import nointro
 import rom
 
+class RomTags(object):
+    def __init__(self, rom):
+        self.total = rom.name.count("(")
+        self.tags = self.__tag_array__(rom.name)
+
+    def __tag_array__(self, name):
+        tags = []
+        end = 0
+        for i in range(self.total):
+            start = name.find("(", end)
+            end = name.find(")", end + 1)
+            tags.extend(self.__strip_tag__(name[start + 1:end].split(",")))
+        return tags
+
+    def __strip_tag__(self, tags):
+        return [tag.strip() for tag in tags]
+
+    def __str__(self):
+        return self.tags.__str__()
+
+class RomCleanName(object):
+    def __init__(self, rom):
+        idx = rom.name.find("(")
+        self.name = rom.name[:idx - 1]
+
+    def __str__(self):
+        return self.name
+
 def load_nointro_roms(dat_files):
     nointro_roms = []
 
@@ -24,3 +52,42 @@ def load_roms(rom_folders):
         roms.extend(rom.load(rom_folder))
 
     return roms
+
+def load_md5_dict(roms):
+    md5_dict = {}
+
+    for rom in roms:
+        md5_dict[rom.md5] = rom
+
+    return md5_dict
+
+def filter_roms(roms, filterss):
+    filtered_roms = []
+
+    for rom in roms:
+        if should_filter(RomTags(rom).tags, filterss):
+            filtered_roms.append(rom)
+
+    return filtered_roms
+
+def should_filter(tags, filterss):
+    ret2 = True
+    for filters in filterss:
+        ret1 = False
+        for filter in filters:
+            if filter.startswith("!"):
+                ret1 |= not tags.__contains__(filter[1:])
+            else:
+                ret1 |= tags.__contains__(filter)
+        ret2 &= ret1
+    return ret2
+
+def unique_filter(roms):
+    unique_names = []
+    unique_roms = []
+    for rom in roms:
+        unique_name = RomCleanName(rom).name
+        if unique_name not in unique_names:
+            unique_names.append(unique_name)
+            unique_roms.append(rom)
+    return unique_roms
